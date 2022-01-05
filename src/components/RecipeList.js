970471a978465d93bdc1287a4a06358f8c03b068
narmanguebraun <narmanguebraun1@gmail.com>
2022-01-05 12:57:33 +0100

    add recipe page

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -8,8 +8,8 @@ export default function RecipeList({ recipes }) {
       {recipes.map(recipe => (
         <div key={recipe.id} className="card">
           <h3>{recipe.title}</h3>
-          <p>{recipe.cookingTime}</p>
-          <div>{recipe.method.substring(0,100)}...</div>
+          <div className="cook-time">{recipe.cookingTime}</div>
+          <p>{recipe.method.substring(0,100)}...</p>
           <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
         </div>
       ))}
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,27 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { error, isPending, data: recipe } = useFetch(url)
+
   return (
-    <div>
-      Recipe
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page_title">{recipe.title}</h2>
+          <div className="cook-time">{recipe.cookingTime}</div>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+        )}
     </div>
   )
 }
