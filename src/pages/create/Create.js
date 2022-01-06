0a197dc6682d2b432bce01dc3c84b8a23c2ef173
narmanguebraun <narmanguebraun1@gmail.com>
2022-01-06 23:59:31 +0100

    redirect user

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
 import './Create.css'
 
@@ -9,6 +10,7 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const history = useHistory()
 
   const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
 
@@ -28,6 +30,13 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
+
+  // redirect the user when we get the data response
+  useEffect(() => {
+    if (data) {
+      history.push('/')
+    }
+  }, [data])
 
   return (
     <div className="create">
